589884db334c8b1ca9543a53e077370ed33f2fef
Haroon <haroon.saeed@outlook.com>
2021-10-13 23:54:04 +0200

    add broadcast

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const io = new Server(server);
 
 app.set("view engine", "ejs");
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.render("home");
 });
 
 io.on("connection", (socket) => {
   console.log("user connected: " + socket.id);
+
+  socket.emit("message", "Welcome to Chatroom!");
+  socket.broadcast.emit("message", "A user has joined!");
+
+  socket.on("disconnect", (arg) => {
+    io.emit("message", "A user has left"); // world
+  });
 });
 
 server.listen(3000, () => {
